9f488620efa7c4b53b8c65ce33e568a46455569d
Anurag Kushwaha <ak4032777@gmail.com>
2025-03-10 22:58:58 +0530

    fixes for dotenv

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const app = express();
+require('dotenv').config()
 const resumeRoutes = require('./routes/resumeRoutes');
 const db = require("./sequelize/models");
 try {
